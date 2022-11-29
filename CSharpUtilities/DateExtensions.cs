b7165e2b06d07079b89e5a4b8a0f46876fa8b122
josegerar <jgarcia24121996@gmail.com>
2022-11-29 00:12:42 -0500

    Agregar archivos de proyecto.

---
+++ CSharpUtilities/DateExtensions.cs
@@ -0,0 +1,65 @@
+﻿namespace CSharpUtilities;
+
+public static class DateExtensions
+{
+    public static DateOnly ToDateOnly(this DateTime source) => DateOnly.FromDateTime(source);
+
+    public static DateOnly? ToDateOnly(this DateTime? source)
+    {
+        if (!source.HasValue)
+        {
+            return null;
+        }
+        try
+        {
+            return source.Value.ToDateOnly();
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
+    public static DateTime? ToDateTime(this DateOnly? source)
+    {
+        if (!source.HasValue)
+        {
+            return null;
+        }
+        try
+        {
+            return source.Value.ToDateTime();
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+
+    public static DateTime ToDateTime(this DateOnly source) => source.ToDateTime(TimeOnly.MinValue);
+
+
+    public static DateTime? ParseDatetimeForTicks(this long datatimelong)
+    {
+        try
+        {
+            return new DateTime(datatimelong);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public static DateTime? ParseDatetimeForSeconds(this long datatimelong)
+    {
+        try
+        {
+            return DateTimeOffset.FromUnixTimeSeconds(datatimelong).DateTime;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
