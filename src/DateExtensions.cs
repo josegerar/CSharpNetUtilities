3cadbe9dc6055260192edb7359767636f0b10f33
josegerar <45176268+josegerar@users.noreply.github.com>
2022-12-28 17:30:28 -0500

    cambos

--- src/DateExtensions.cs
+++ src/DateExtensions.cs
@@ -6,41 +6,15 @@ namespace CSharpNetUtilities
     {
 #if NET6_0_OR_GREATER
 
-public static DateOnly ToDateOnly(this DateTime source) => DateOnly.FromDateTime(source);
-
-        public static DateOnly? ToDateOnly(this DateTime? source)
-        {
-            if (!source.HasValue)
-            {
-                return null;
-            }
-            try
-            {
-                return source.Value.ToDateOnly();
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-
-        public static DateTime? ToDateTime(this DateOnly? source)
-        {
-            if (!source.HasValue)
-            {
-                return null;
-            }
-            try
-            {
-                return source.Value.ToDateTime();
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
+        public static DateOnly ToDateOnly(this DateTime source) => DateOnly.FromDateTime(source);
 
         public static DateTime ToDateTime(this DateOnly source) => source.ToDateTime(TimeOnly.MinValue);
+
+        public static DateOnly? ToDateOnly(this DateTime? source) => !source.HasValue ? null : source.Value.ToDateOnly();
+
+        public static DateTime? ToDateTime(this DateOnly? source) => !source.HasValue ? null : source.Value.ToDateTime();
+
+        
 
 #endif
 
