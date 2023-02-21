913cfb9a49fe6c32da2d96d2b6189bc947c98c18
josegerar <jgarcia24121996@gmail.com>
2023-02-21 11:57:43 -0500

    v1.1
    Add multiple constants in the Constants struct

---
+++ src/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharpNetUtilities
+{
+    public struct Constants
+    {
+        public const string TotalRecords = "records";
+        public const string TotalPages = "pages";
+
+        public struct Format
+        {
+            public const string DateOnly = "yyyy-MM-dd";
+            public const string TimeOnly = "HH:mm:ss.FFFFFFF";
+            public const string DateTime = "yyyy-MM-ddTHH:mm:ss.fff";
+            public const string DateTimeOffset = "yyyy-MM-ddTHH:mm:ss.fff";
+        }
+    }
+}
+
